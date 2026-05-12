51cacf20e860b90b9818393bb8812778222e2df5
Vitor Caitano <caitanoovitor@gmail.com>
2026-05-12 16:42:12 -0300

    Implementação da opção de Saque

--- ContaCorrente.ConsoleApp/Program.cs
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -1,1 +1,51 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+int id = 1;
+decimal saldo = 2200;
+decimal limiteDebito = 1000;
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("---------------------------------------------");
+    Console.WriteLine($"Conta Corrente");
+    Console.WriteLine("---------------------------------------------");
+    Console.WriteLine("1 - Saque");
+    Console.WriteLine("2 - Depósito");
+    Console.WriteLine("3 - Consulta de Saldo");
+    Console.WriteLine("S - Sair");
+    Console.WriteLine("---------------------------------------------");
+    Console.Write("> ");
+    string? opcaoMenu = Console.ReadLine();
+
+    if (opcaoMenu?.ToUpper() == "S")
+        break;
+
+    if (opcaoMenu == "1")
+    {
+        Console.WriteLine("Digite o valor do Saque: R$");
+        decimal valorSaque = Convert.ToDecimal(Console.ReadLine());
+
+        decimal limiteSaque = saldo + limiteDebito;
+
+        if (valorSaque > limiteSaque)
+        {
+            Console.WriteLine("Não é possivel Fazer o saque, SALDO insuficiente! ");
+            Console.ReadLine();
+            continue;
+        }
+
+        saldo -= valorSaque;
+
+        Console.WriteLine($"O saque da R$ {valorSaque} foi efetuada com sucesso! ");
+        Console.ReadLine();
+
+    }
+    else if (opcaoMenu == "2")
+    {
+
+    }
+    else if (opcaoMenu == "3")
+    {
+
+    }
+}
--- ContaCorrente.ConsoleApp/obj/Debug/net10.0/ContaCorrente.ConsoleApp.AssemblyInfo.cs
+++ ContaCorrente.ConsoleApp/obj/Debug/net10.0/ContaCorrente.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContaCorrente.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+190df8e95ab7538dda15a9f6ca6b507d047d1881")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContaCorrente.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContaCorrente.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
